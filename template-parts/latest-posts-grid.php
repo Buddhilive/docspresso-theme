c23cceee74c9312816c943026359581da42da401
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-05-21 22:00:00 +0530

    Refactor related article section

--- template-parts/latest-posts-grid.php
+++ template-parts/latest-posts-grid.php
@@ -66,6 +66,12 @@ if ($latest_posts->have_posts()) :
                                 <?php the_post_thumbnail('medium_large', array('class' => 'w-full h-full object-cover hover:scale-105 transition-transform duration-300')); ?>
                             </a>
                         </div>
+                    <?php else : ?>
+                        <div class="aspect-video bg-gradient-to-br from-purple-100 via-blue-100 to-purple-100 flex items-center justify-center overflow-hidden">
+                            <svg class="w-16 h-16 text-purple-300" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"></path>
+                            </svg>
+                        </div>
                     <?php endif; ?>
                     
                     <div class="p-6">
